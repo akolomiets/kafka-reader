f529101cc8a0ddb8b88e0cf9e89988cb79200d11
Andrey Kolomiets <andrey.npk@gmail.com>
2026-08-04 22:17:48 +0300

    SVG Spinner added to the busy panel

--- src/main/kotlin/com/npk/kfv/components/JBusyPanel.kt
+++ src/main/kotlin/com/npk/kfv/components/JBusyPanel.kt
@@ -1,7 +1,12 @@
 package com.npk.kfv.components
 
-import com.formdev.flatlaf.ui.FlatLineBorder
+import com.github.weisj.jsvg.parser.SVGLoader
+import com.github.weisj.jsvg.renderer.awt.AwtComponentPlatformSupport
+import com.github.weisj.jsvg.renderer.output.Output
+import com.github.weisj.jsvg.ui.AnimationPlayer
+import com.github.weisj.jsvg.view.ViewBox
 import com.npk.kfv.StopwatchTimer
+import com.npk.kfv.requireResource
 import com.npk.kfv.synchronized
 import java.awt.*
 import java.awt.event.MouseAdapter
@@ -9,6 +14,40 @@ import java.awt.event.MouseEvent
 import javax.swing.*
 
 class JBusyPanel : JPanel(GridBagLayout()) {
+
+    private class SpinnerComponent : JComponent() {
+
+        private val document = requireNotNull(SVGLoader().load(requireResource("/ico/spinner.svg")))
+        private val player = AnimationPlayer({ parent?.repaint() }).also { it.setAnimation(document.animation()) }
+
+        override fun getWidth(): Int = 64
+
+        override fun getHeight(): Int = 64
+
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+            (g as Graphics2D).let { g2d ->
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+                g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE)
+
+                document.renderWithPlatform(
+                    AwtComponentPlatformSupport(this),
+                    Output.createForGraphics(g2d),
+                    ViewBox(0f, 0f, getWidth().toFloat(), getHeight().toFloat()),
+                    player.animationState()
+                )
+            }
+        }
+
+        fun startAnimation() {
+            player.start()
+        }
+
+        fun stopAnimation() {
+            player.stop()
+        }
+
+    }
 
     companion object {
         private const val STOPWATCH_LABEL = "%02d : %02d"
@@ -20,30 +59,49 @@ class JBusyPanel : JPanel(GridBagLayout()) {
             dialog.glassPane as JBusyPanel
     }
 
-    private val progressBar = JProgressBar().also {
-        it.isStringPainted = true
-        it.isIndeterminate = true
-        it.string = ""
-        it.preferredSize = Dimension(it.preferredSize.width, it.preferredSize.height + 8)
-    }
+    private val spinnerComponent = SpinnerComponent()
+    private val stopwatchLabel = JLabel()
     private val progressLabel = JLabel()
 
     private val stopwatchTimer = StopwatchTimer { seconds ->
-        progressBar.string = STOPWATCH_LABEL.format((seconds % 3600) / 60, seconds % 60)
+        stopwatchLabel.text = STOPWATCH_LABEL.format((seconds % 3600) / 60, seconds % 60)
     }
 
     init {
         isOpaque = false
-        val panel = JPanel(GridBagLayout()).also { panel ->
-            val constraint = GridBagConstraints().apply { insets = Insets(15, 25, 0, 25) }
-            panel.border = FlatLineBorder(Insets(5, 5, 5, 5), Color.LIGHT_GRAY, 1.0f, 16)
-            panel.add(progressBar, constraint.apply { gridy++ })
-            panel.add(progressLabel, constraint.apply {
-                gridy++
-                insets.bottom = 15
-            })
+
+        val constraints = GridBagConstraints()
+
+        constraints.apply {
+            gridx = 0
+            gridy = 0
+            weightx = 1.0
+            weighty = 1.0
+            anchor = GridBagConstraints.CENTER
+            insets = Insets(0, -spinnerComponent.width, spinnerComponent.height, 0)
         }
-        add(panel)
+        add(spinnerComponent, constraints)
+
+        constraints.apply {
+            gridx = 0
+            gridy = 1
+            weightx = 1.0
+            weighty = 0.0
+            anchor = GridBagConstraints.PAGE_END
+            insets = Insets(0, 0, 5, 0)
+        }
+        add(stopwatchLabel, constraints)
+
+        constraints.apply {
+            gridx = 0
+            gridy = 2
+            weightx = 1.0
+            weighty = 0.0
+            anchor = GridBagConstraints.PAGE_END
+            insets = Insets(0, 0, 20, 0)
+        }
+        add(progressLabel, constraints)
+
         addMouseListener(object : MouseAdapter() {
             override fun mousePressed(event: MouseEvent) {
                 event.consume()
@@ -63,7 +121,8 @@ class JBusyPanel : JPanel(GridBagLayout()) {
             isVisible = true
             isRunning = true
 
-            progressBar.string = STOPWATCH_LABEL.format(0, 0)
+            spinnerComponent.startAnimation()
+            stopwatchLabel.text = STOPWATCH_LABEL.format(0, 0)
             stopwatchTimer.reset()
             stopwatchTimer.start()
         }
@@ -75,15 +134,31 @@ class JBusyPanel : JPanel(GridBagLayout()) {
             isVisible = false
 
             stopwatchTimer.stop()
-            progressBar.string = ""
+            stopwatchLabel.text = ""
+            spinnerComponent.stopAnimation()
         }
     }
 
     override fun paintComponent(g: Graphics) {
         (g as Graphics2D).let { g2d ->
+            val oldPainter = g2d.paint
+
             g2d.color = background.let { Color(it.red, it.green, it.blue, 100) }
             g2d.fillRect(0, 0, width, height)
-            super.paintComponent(g2d)
+
+            g2d.paint = GradientPaint(
+                0f, (height - 220).toFloat(), background.let { Color(it.red, it.green, it.blue, 0) },
+                0f, height.toFloat(), background.let { Color(it.red, it.green, it.blue, 255) }
+            )
+            g2d.fillRect(0, height - 220, width, 220)
+
+            g2d.paint = GradientPaint(
+                0f, (height - 100).toFloat(), background.let { Color(it.red, it.green, it.blue, 0) },
+                0f, height.toFloat(), background.let { Color(it.red, it.green, it.blue, 255) }
+            )
+            g2d.fillRect(0, height - 100, width, 100)
+
+            g2d.paint = oldPainter
         }
     }
 
